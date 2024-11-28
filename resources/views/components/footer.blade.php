87e6d4dd1cd0dfd7a733b7d88cffd2b06eb1d928
Zimhiii <hijazmih@gmail.com>
2024-11-28 21:25:52 +0700

    make dashboard view

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="font-nunito px-[82px] bg-[#060606] border-t-[0.5px] border-[#C9A840]">
+    <div class="pt-[46px] flex justify-between pb-[71px] border-b-[0.5px] border-[#BFB9A5]">
+      <div class="flex flex-col gap-[8px]">
+        <div class="w-[72px] h-[72px] bg-slate-500 rounded-full"></div>
+        <h1 class="font-bold text-[25px] text-[#EDE5CE]">HIMATEPA ITERA</h1>
+      </div>
+      <div class="flex gap-[80px]">
+        <div class="flex flex-col gap-[8px] text-[16px] font-medium text-[#EDE5CE]">
+          <h2 class="font-bold text-[20px]">Kenali Kami</h2>
+          <h3>Kabinet Sahityadhinata </h3>
+          <h3>Teknologi Pangan ITERA </h3>
+          <h3>Institut Teknologi Sumatera </h3>
+        </div>
+        <div class="flex flex-col gap-[8px] text-[16px] font-medium text-[#EDE5CE]">
+          <h2 class="font-bold text-[20px]">Telusuri</h2>
+          <h3>Tentang HIMATEPA </h3>
+          <h3>Produk HIMATEPA</h3>
+          <h3>Berita HIMATEPA </h3>
+          <h3>Beasiswa</h3>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-between py-[28px]">
+      <div>
+        <p class="text-[13px] font-bold font-nunito text-[#EDE5CE]">
+          &copy; Copyright &copy;2024 | Himpunan Mahasiswa Teknologi
+          Pangan ITERA
+        </p>
+      </div>
+      <div class="flex gap-[56px]">
+        <div class="bg-tik-tok w-[24px] h-[24px] bg-cover bg-center"></div>
+        <div class="bg-e-mail w-[24px] h-[24px] bg-cover bg-center"></div>
+        <div class="bg-linked-in w-[24px] h-[24px] bg-cover bg-center"></div>
+        <div class="bg-insta-gram w-[24px] h-[24px] bg-cover bg-center"></div>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,48 @@
+<div class="relative h-[937px] w-full overflow-hidden -mt-[112px] bg-[#060606]">
+    <div class="absolute bg-hero-dashboard h-[937px]  inset-0  bg-cover bg-center "></div>
+    <div class="absolute inset-0 h-[937px] w-full bg-gradient-to-b from-[#000000]/0 from-10%  to-[#181818] to-95%"></div>
+    <div class="absolute inset-0 bg-black bg-opacity-75 h-[937px] "></div>
+    <div class="relative text-white">
+      <div class="mt-[256px] ml-[80px] text-[#DAD3BE] ">
+        <h1 class="text-[96px] font-black  font-segoeBold h-fit">
+          SELAMAT DATANG
+          {{-- {{ $title }} --}}
+        </h1>
+        <h2 class="text-[24px] font-semibold max-w-[600px] font-nunito">
+          Website Resmi Himpunan Mahasiswa Ganasarkara Institut Teknologi
+          Sumatera
+          {{-- {{ $description }} --}}
+        </h2>
+      </div>
+      <div class="relative font-segoeBold text-[24px] text-[#4D4214] font-black flex flex-col justify-center items-center mt-[230px]">
+        <div class="absolute z-1 flex gap-[32px] justify-evenly bg-[#C9A840] py-[15px] rotate-[4deg] overflow-hidden ">
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+        </div>
+        <div class="absolute z-2 flex gap-[32px] justify-evenly bg-[#C9A840] py-[15px] rotate-[-4deg] overflow-hidden ">
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+          <h2>HIMATEPA</h2>
+        </div>
+      </div>
+    </div>
+  </div>
